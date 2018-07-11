5c7b9d6ab83c3955f381e088c3cd3f7399db3ce3
davidcharlat <39385280+davidcharlat@users.noreply.github.com>
2018-07-11 22:10:44 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ gcc -o  test01s ft_strncpy.test.c j05/ex01/ft_strncpy.c
 gcc -o  test02s ft_strstr.test.c j05/ex02/ft_strstr.c
 gcc -o  test03s ft_strcmp.test.c j05/ex03/ft_strcmp.c
 gcc -o  test04s ft_strncmp.test.c j05/ex04/ft_strncmp.c
-gcc -o  test05s ft_strncmp.test.c j05/ex05/ft_strncmp.c
+gcc -o  test05s ft_strncmp.test.c j05/ex05/ft_strupcase.c
 ./test00s
 ./test01s
 ./test02s
