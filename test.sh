875cdaccba4230599b8becd6516e294a464f725e
davidcharlat <39385280+davidcharlat@users.noreply.github.com>
2018-07-15 19:16:47 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
-gcc -o  test00s ft_strcpy.test.c j05/ex00/ft_strcpy.c
-gcc -o  test01s ft_strncpy.test.c j05/ex01/ft_strncpy.c
-gcc -o  test02s ft_strstr.test.c j05/ex02/ft_strstr.c
-gcc -o  test03s ft_strcmp.test.c j05/ex03/ft_strcmp.c
-gcc -o  test04s ft_strncmp.test.c j05/ex04/ft_strncmp.c
-gcc -o  test05s ft_strupcase.test.c j05/ex05/ft_strupcase.c
-./test00s
-./test01s
-./test02s
-./test03s
-./test04s
-./test05s
+#!/bin/bash
+export  J05="https://github.com/davidcharlat/j05"
+git clone $J05
+let "n = 0"
+for file in 'ft_strcpy' 'ft_strncpy' 'ft_strstr' 'ft_strcmp' 'ft_strncmp' 'ft_strupcase'
+do	
+	if [ -f j05/ex0$n/$file.c ]
+	then 
+		gcc -o test$n $file.test.c j05/ex0$n/$file.c
+		./test$n
+		let "n = n + 1"
+	fi
+done
+
 
