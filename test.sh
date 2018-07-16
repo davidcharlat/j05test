737c38a7baaf1571557bfd5d884405eaa7e22839
davidcharlat <david_charlat@yahoo.fr>
2018-07-16 19:31:19 +0200

    test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
-gcc -o  test00s ft_strcpy.test.c j05/ex00/ft_strcpy.c
-gcc -o  test01s ft_strncpy.test.c j05/ex01/ft_strncpy.c
-gcc -o  test02s ft_strstr.test.c j05/ex02/ft_strstr.c
-gcc -o  test03s ft_strcmp.test.c j05/ex03/ft_strcmp.c
-gcc -o  test04s ft_strncmp.test.c j05/ex04/ft_strncmp.c
-gcc -o  test05s ft_strncmp.test.c j05/ex05/ft_strncmp.c
-./test00s
-./test01s
-./test02s
-./test03s
-./test04s
-./test05s
+let "n = 0"
+nfile=$(find j05 -type f ! -regex '.*/\..*' -printf "0"|wc -c)
 
+for file in 'ft_strcpy' 'ft_strncpy' 'ft_strstr' 'ft_strcmp' 'ft_strncmp' 'ft_strupcase' 'ft_strlowcase' 'ft_strcapitalize' 'ft_str_is_alpha' 'ft_str_is_numeric'
+do	
+	if [ -f j05/ex0$n/$file.c ]
+	then 
+		gcc -o test$n $file.test.c j05/ex0$n/$file.c
+		./test$n
+		let "n = n + 1"
+	fi
+done
+if [ $n -eq $nfile ]
+then
+	echo "resultat = $n"
+else
+	echo "presence de fichiers autres que ceux specifies dans l'enonce -> resultat = 0"
+fi
